59f71de351bc10170864d838abb5a4c9ca8cca0b
vbrooke78 <vscotter@hotmail.com>
2022-04-19 15:11:53 +0100

    this commit will make minor changes to Articles.jsx file

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import { getArticles } from '../utils/api';
 
 const Articles = () => {
-  getArticles();
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
@@ -18,10 +17,10 @@ const Articles = () => {
         return (
           <li className="listItems" key={article.article_id}>
             <h2>{article.title}</h2>
-            <h3>Author: {article.author}</h3>
-            <h3>Topic: {article.topic}</h3>
-            <h4>Votes: {article.votes}</h4>
-            <h4>Comments: {article.comment_count}</h4>
+            <p>Author: {article.author}</p>
+            <p>Topic: {article.topic}</p>
+            <p>Votes: {article.votes}</p>
+            <p>Comments: {article.comment_count}</p>
           </li>
         );
       })}
