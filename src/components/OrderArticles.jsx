25f57d791ade8ac3af83d1a4bb3e71ab42faa38e
vbrooke78 <vscotter@hotmail.com>
2022-04-22 13:46:56 +0100

    this commit will enable the user to sort the articles by author, comment count, date and votes and order by asc or desc

---
+++ src/components/OrderArticles.jsx
@@ -0,0 +1,28 @@
+const OrderArticles = ({ setOrderBy }) => {
+  const validOrderBys = [
+    { name: 'Desc', apiName: 'desc' },
+    { name: 'Asc', apiName: 'asc' },
+  ];
+
+  const handleChange = (e) => {
+    setOrderBy(e.target.value);
+  };
+
+  return (
+    <>
+      <label htmlFor="order-by">Order by:</label>
+      <select className="dropdown" id="sort-by" onChange={handleChange}>
+        <option>--select--</option>
+        {validOrderBys.map((order_by) => {
+          return (
+            <option key={order_by.name} value={order_by.apiName}>
+              {order_by.name}
+            </option>
+          );
+        })}
+      </select>
+    </>
+  );
+};
+
+export default OrderArticles;
